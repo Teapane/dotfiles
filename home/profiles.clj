b9988f997df40c30c7fd40811f5639b83a1db88b
Teapane <tyler.stephen.long@gmail.com>
2015-08-25 08:56:36 -0600

    add lein profile--minor start

---
+++ home/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:repl-options {:prompt (fn [ns] (str "\033[1;32m" ns "=>" "\033[0m "))}}}
+
+{:user {:plugins [[venantius/ultra "0.3.4"]]
+                :ultra {:color-scheme :solarized_dark}}}
